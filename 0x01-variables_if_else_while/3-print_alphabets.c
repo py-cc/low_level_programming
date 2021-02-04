31736118dde65becea9d035fd02b892ac91c3e36
py-cc <2227@holbertonschool.com>
2021-02-04 18:43:33 +0000

    betty style

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,13 +6,13 @@
 **/
 int main(void)
 {
-	char lower;
-	char cap;
-	for(lower = 'a'; lower <= 'z'; lower++)
+	char lower, cap;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
 	{
 		putchar(lower);
 	}
-	for(cap = 'A'; cap <= 'Z'; cap++)
+	for (cap = 'A'; cap <= 'Z'; cap++)
 	{
 		putchar(cap);
 	}
