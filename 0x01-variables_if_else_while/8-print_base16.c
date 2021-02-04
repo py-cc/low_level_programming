9c80a7da37f117c8d67c5b86eef5e401c68b3231
py-cc <2227@holbertonschool.com>
2021-02-04 19:04:52 +0000

    print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+**/
+int main(void)
+{
+
+	int i;
+	char a;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+
+	for (a = 'a'; a <= 'f'; a++)
+		putchar(a);
+
+	putchar('\n');
+	return (0);
+}
