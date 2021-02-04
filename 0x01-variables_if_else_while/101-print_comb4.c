1fd019f5fde58c2d7fe7c7392ad551a790d1608f
py-cc <2227@holbertonschool.com>
2021-02-04 21:50:23 +0000

    betty style

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,33 +6,34 @@
 **/
 int main(void)
 {
-int i, j, x;
-for (i = 0; i < 10; i++)
-{
-for (j = 0; j < 10; j++)
-{
-for (x = 0; x < 10; x++)
-{
-if (!(i < j && i < x))
-continue;
-if (j == x)
-continue;
-if (j > x)
-{
-continue;
-}
-else
-{
-putchar(i + '0');
-putchar(j + '0');
-putchar(x + '0');
-if (!(j == 8 && x == 9 && i == 7))
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-putchar('\n');
-return (0);
+	int i, j, x;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (x = 0; x < 10; x++)
+			{
+				if (!(i < j && i < x))
+				continue;
+				if (j == x)
+				continue;
+				if (j > x)
+				{
+				continue;
+				}
+				else
+				{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(x + '0');
+				if (!(j == 8 && x == 9 && i == 7))
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
