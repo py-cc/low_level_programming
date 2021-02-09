76dae0abb9b725d4dddf59541f7d0a3899635ef6
py-cc <2227@holbertonschool.com>
2021-02-09 03:44:13 +0000

    files

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+* print_sign - prints the sign of a number
+* @n: num to be checked
+*
+* Return: 1 if n is greater than zero. 0 if n is zero. -1 if less than zero.
+*/
+int print_sign(int n)
+{
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
