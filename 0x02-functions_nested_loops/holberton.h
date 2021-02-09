fe0114f5f10403390e7ab1f478d39c8f50eed582
py-cc <2227@holbertonschool.com>
2021-02-09 12:25:39 +0000

    library

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,7 @@
+int _putchar(char c); /* own putchar function */
+void print_alphabet(void); /* print alphabet whit putchar*/
+void print_alphabet_x10(void); /* print alphabet ten times*/
+int _islower(int c); /* check for lowercase character*/
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
