95469895553e6362fe5863b6de880b3b55c2b745
py-cc <2227@holbertonschool.com>
2021-02-09 23:08:39 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+* add - adds two integers and return result
+*
+* Return: Result with add
+*/
+
+int add(int n, int n2)
+{
+	int sum;
+
+	sum = n + n2;
+
+	return (sum);
+}
