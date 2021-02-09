cfe823d5527451277813b21aa937a8eab7cc0e74
py-cc <2227@holbertonschool.com>
2021-02-09 17:15:11 +0000

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,31 +1,24 @@
 #include "holberton.h"
 
-/**
-* jack_bauer - prints every minute
-*
-*/
-
 void jack_bauer(void)
 {
-
-	int min, min2, hr, hr2;
-
-	for (hr = 0; hr <= 2; hr++)
-	{
-		for (hr2 = 0; hr2 <= 3; hr2++)
-		{
-			for (min = 0; min <= 5; min++)
-			{
-				for (min2 = min; min2 < 10; min2++)
-				{
-					_putchar(hr + '0');
-					_putchar(hr2 + '0');
-					_putchar(':');
-					_putchar(min + '0');
-					_putchar(min2 + '0');
-					_putchar('\n');
-				}
-			}
-		}
-	}
+int min, min2, hr, hr2;
+for (hr = 0; hr < 3; hr++ )
+{
+for (hr2 = 0; hr2 < 4; hr2++)
+{
+for (min = 0; min < 6; min++)
+{
+for (min2 = 0; min2 < 10; min2++)
+{
+_putchar(hr + '0');
+_putchar(hr2 + '0');
+_putchar(':');
+_putchar(min + '0');
+_putchar(min2 + '0');
+_putchar('\n');
+}
+}
+}
+}
 }
