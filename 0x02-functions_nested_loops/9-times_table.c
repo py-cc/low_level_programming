d6603214f9919433f81dbbf99c0ec8c35df79129
py-cc <2227@holbertonschool.com>
2021-02-09 23:03:43 +0000

    table nine

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+/**
+* times_table - print the 9 times table
+*
+*/
+
+void times_table(void)
+{
+int i, j, mult;
+for (i = 0; i <= 9; i++)
+{
+	for (j = 0; j <= 9; j++)
+	{
+
+	mult = i * j;
+
+	if (j == 0)
+	{
+	_putchar(0 + '0');
+	}
+	else if (mult / 10 == 0 && j != 0)
+	{
+		_putchar(' ');
+		_putchar(mult % 10 + '0');
+	}
+	else
+	{
+		_putchar(mult / 10 + '0');
+		_putchar(mult % 10 + '0');
+	}
+
+	if (j != 9)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+	}
+	}
+}
