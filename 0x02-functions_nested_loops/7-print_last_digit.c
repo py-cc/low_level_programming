b4ad34c04f3b54664781a325955238a210c3f4a8
py-cc <2227@holbertonschool.com>
2021-02-09 13:24:28 +0000

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,17 +10,15 @@
 int print_last_digit(int a)
 {
 
-	a = a % 10;
-
-	if (a < 0)
+	if (a % 10 < 0)
 	{
 		a = a * -1;
-		_putchar(a + '0');
+		_putchar(a % 10 + '0');
 	}
 	else
 	{
-		_putchar(a + '0');
+		_putchar(a % 10 + '0');
 	}
 
-	return (a);
+	return (a % 10);
 }
