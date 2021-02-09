78f02f734235f3ccd7e19ec8f0e5820cb75e0f57
py-cc <2227@holbertonschool.com>
2021-02-09 02:00:06 +0000

    print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+* print_alphabet_x10 - print alphabet ten times using my own function
+*/
+
+void print_alphabet_x10(void)
+{
+
+	int num;
+
+	for (num = 0; num <= 10; num++)
+	{
+		print_alphabet();
+	}
+
+}
+
+/**
+* print_alphabet - print alphabet in lowercase
+*
+*/
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
