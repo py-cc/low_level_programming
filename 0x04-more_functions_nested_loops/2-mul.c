44e4745c10eb2249caee6bc906d5bda068f97ea2
py-cc <2227@holbertonschool.com>
2021-02-11 14:13:09 +0000

    files functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * mul - multiplies two integers
+ * @a: Firts number check
+ * @b: Second number check
+ *
+ * Return: result
+ */
+int mul(int a, int b)
+{
+
+	int result = 0;
+
+	result = a * b;
+
+	return(result);
+}
