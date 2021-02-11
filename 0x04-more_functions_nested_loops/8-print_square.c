cdcc22dc9bf19d66d6c8433501738e716f91f7ce
py-cc <2227@holbertonschool.com>
2021-02-11 17:02:45 +0000

    print squeare

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * print_square - prints a square
+ *
+ * @size: is the size  of the squeare '#' should be printed
+ */
+void print_square(int size)
+{
+
+	int row, column;
+
+	if (size > 0)
+	{
+		for (row = 0; row < size; row++)
+		{
+			for (column = 0; column < size; column++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+}
