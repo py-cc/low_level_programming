1c05ea1c7c64217f4231b4f093b3d810b088625c
py-cc <2227@holbertonschool.com>
2021-02-11 17:36:10 +0000

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* main - Entry Point
+*
+* Returns: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+			printf("FizzBuzz ");
+		else if (num % 5 == 0)
+			printf("Buzz ");
+		else if (num % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", num);
+	}
+	printf("\n");
+	return (0);
+}
