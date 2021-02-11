a0976f82f6387343727aa77c13d908d00515710d
py-cc <2227@holbertonschool.com>
2021-02-11 14:46:06 +0000

    files

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_line - prints the numbers from 0 to 14
+ *
+ * @n: is the number of times the character _ should be printed
+ */
+void print_line(int n)
+{
+
+	if (n > 0)
+	{
+		int i;
+
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
