10891e462e1159c12c9f35729ca1c6d3048c8d23
py-cc <2227@holbertonschool.com>
2021-02-11 21:41:15 +0000

    print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * print_triangle - prints a triangle
+ *
+ * @size: is the size  of the triangle '#' should be printed
+ */
+
+void print_triangle(int size)
+{
+
+	int x = 0, y = 0;
+
+	for (x = 1; x <= size; x++)
+	{
+		for (y = x; y < size; y++)
+		{
+			_putchar(' ');
+
+		}
+
+		for (y = 1; y <= x; y++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+
+}
