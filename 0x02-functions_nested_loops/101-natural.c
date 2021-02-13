ca33cca20691d4af6ac43132e865a12a46b0caff
py-cc <2227@holbertonschool.com>
2021-02-13 22:44:38 +0000

    number natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	int sum = 0, i;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
