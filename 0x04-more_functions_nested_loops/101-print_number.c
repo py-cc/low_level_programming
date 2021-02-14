2974a9b7b947e26f802477c34940d58a990adb7a
py-cc <2227@holbertonschool.com>
2021-02-14 18:22:42 +0000

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,48 +1,49 @@
 #include "holberton.h"
 /**
-* print_number - prints an integer whit putchar
+* print_number - prints an integer
 * @n: check number
 *
+* Return: Always 0 (Success)
 */
 void print_number(int n)
 {
 unsigned int num;
-if (n < 100)
+if (n > 0)
 {
-if (n < 0)
+if (n > 100 && n < 1000)
 {
-n = -n;
-_putchar('-');
+putchar(n / 100 + '0');
 num = n / 10;
-_putchar(num + '0');
-num = n % 10;
-_putchar(num + '0');
-}
-else if (n == 0)
-{
-_putchar(n + '0');
-}
-else
-{
-_putchar(n / 10 + '0');
-num = n % 10;
-_putchar(num + '0');
-}
-}
-else if (n > 100 && n < 1000)
-{
-_putchar(n / 100 + '0');
-num = n / 10;
-_putchar(num % 10 + '0');
-_putchar(n % 10 + '0');
+putchar(num % 10 + '0');
+putchar(n % 10 + '0');
 }
 else if (n > 1000)
 {
-_putchar(n / 1000 + '0');
+putchar(n / 1000 + '0');
 num = n / 100;
-_putchar(num % 10 + '0');
+putchar(num % 10 + '0');
 num = n % 100;
-_putchar(num / 10 + '0');
-_putchar(num % 10 + '0');
+putchar(num / 10 + '0');
+putchar(num % 10 + '0');
+}
+else
+{
+putchar(n / 10 + '0');
+num = n % 10;
+putchar(num + '0');
+}
+}
+else if (n < 0)
+{
+n = -n;
+putchar('-');
+num = n / 10;
+putchar(num + '0');
+num = n % 10;
+putchar(num + '0');
+}
+else if (n == 0)
+{
+putchar(n + '0');
 }
 }
