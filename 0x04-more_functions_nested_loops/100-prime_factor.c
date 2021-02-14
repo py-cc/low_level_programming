1698a3d53aa8d92e99a3df7346a564ed00edd35c
py-cc <2227@holbertonschool.com>
2021-02-14 12:43:13 +0000

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry Point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	long n = 612852475143;
+	long prime;
+
+	for (prime = 2; n > prime; prime++)
+	{
+		while(n % prime == 0)
+		{
+			n = n / prime;
+		}
+	}
+	printf("%lu\n", prime);
+	return (0);
+}
