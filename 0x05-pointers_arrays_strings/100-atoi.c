8d33ce57e12daa47095114b16d8764b16fe7a545
py-cc <2227@holbertonschool.com>
2021-02-16 23:38:44 +0000

    test

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,16 +11,13 @@ int _atoi(char *s)
 
 	for (len = 0; s[len] != '\0'; len++)
 	{
-		if (s[len] > 0 || s[len] < 0)
-		{
-			if (s[len] == 45)
+		if (s[len] == 45)
 			signo++;
-			else if (s[len] >= '0' && s[len] <= '9')
+		else if (s[len] >= '0' && s[len] <= '9')
 			result = result * 10 + s[len] - 48;
-		}
 		else if (*s == '\0')
 			return (0);
-		}
+	}
 
 	return (signo % 2 == 1 ? result = -result : result);
 }
