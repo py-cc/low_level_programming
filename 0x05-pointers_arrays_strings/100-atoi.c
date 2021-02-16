0c35ede1159c9e3a2fe62b4cbd0af923a5fbd802
py-cc <2227@holbertonschool.com>
2021-02-16 23:26:26 +0000

    style betty

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,21 +1,23 @@
 #include "holberton.h"
-
+/**
+* _atoi - convert a string to an integer
+* @s: pointer to string check
+* Return: integer convert
+*/
 int _atoi(char *s)
 {
-  int len, result = 0, signo = 0;
-
-  for (len = 0; s[len] != '\0'; len++)
-  {
-    if (s[len] > 0 || s[len] < 0)
-    {
-      if (s[len] == 45)
-        signo++;
-      else if (s[len] >= '0' && s[len] <= '9')
-          result = result * 10 + s[len] - 48;
-    }
-    else if (s[len] == 0)
-      return 0;
-  }
-
-  return signo%2 == 1 ? result = -result : result;
+int len, result = 0, signo = 0;
+for (len = 0; s[len] != '\0'; len++)
+{
+if (s[len] > 0 || s[len] < 0)
+{
+if (s[len] == 45)
+signo++;
+else if (s[len] >= '0' && s[len] <= '9')
+result = result * 10 + s[len] - 48;
+}
+else if (s[len] == 0)
+return (0);
+}
+return (signo % 2 == 1 ? result = -result : result);
 }
