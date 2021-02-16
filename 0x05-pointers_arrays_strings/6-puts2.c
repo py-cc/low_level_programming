f36ff392ac7afcff790491b9e364c616bcafe1a6
py-cc <2227@holbertonschool.com>
2021-02-16 15:24:52 +0000

    add file to check

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,13 +6,18 @@
 */
 void puts2(char *str)
 {
-	int i;
+	int i, j, count = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] % 2 == 0)
+		count++;
+	}
+
+	for (j = 0; j <= count; j++)
+	{
+		if (j % 2 == 0)
 		{
-			_putchar(str[i]);
+			_putchar(str[j]);
 		}
 	}
 	_putchar('\n');
