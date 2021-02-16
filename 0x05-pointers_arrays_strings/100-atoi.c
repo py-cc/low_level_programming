35d3af5ffd678b251ccdac38e6da86e949e36957
py-cc <2227@holbertonschool.com>
2021-02-16 23:36:47 +0000

    unsigned int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,18 +6,21 @@
 */
 int _atoi(char *s)
 {
-int len, result = 0, signo = 0;
-for (len = 0; s[len] != '\0'; len++)
-{
-if (s[len] > 0 || s[len] < 0)
-{
-if (s[len] == 45)
-signo++;
-else if (s[len] >= '0' && s[len] <= '9')
-result = result * 10 + s[len] - 48;
-}
-else if (s[len] == 0)
-return (0);
-}
-return (signo % 2 == 1 ? result = -result : result);
+	unsigned int len, result = 0;
+	int signo = 0;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+		if (s[len] > 0 || s[len] < 0)
+		{
+			if (s[len] == 45)
+			signo++;
+			else if (s[len] >= '0' && s[len] <= '9')
+			result = result * 10 + s[len] - 48;
+		}
+		else if (*s == '\0')
+			return (0);
+		}
+
+	return (signo % 2 == 1 ? result = -result : result);
 }
