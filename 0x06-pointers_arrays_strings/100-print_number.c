50f61cbca7f26e558459d79562ebdfdc24f16748
py-cc <2227@holbertonschool.com>
2021-02-22 03:53:04 +0000

    add file to check

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+* print_number - prints an integer
+*
+* @n: num to check
+*/
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar(n % 10 + '0');
+}
