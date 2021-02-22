2cb23ac9dd67bad2bb015efaa454718bce817642
py-cc <2227@holbertonschool.com>
2021-02-22 19:30:14 +0000

    add file to check

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,17 +9,13 @@
 */
 char *_strchr(char *s, char c)
 {
-
 	while (*s != '\0')
 	{
 		if (*s == c)
 		{
 			return (s);
 		}
-		else if (*s == '\0')
-		{
-			return ('\0');
-		}
 		s++;
 	}
+	return ('\0');
 }
