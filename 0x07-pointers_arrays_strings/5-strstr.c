391b37d0d48c0c2d7db34ca04968ef3c538528df
py-cc <2227@holbertonschool.com>
2021-02-22 23:40:40 +0000

    add file to check

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+* _strstr - locates a substring
+* @haystack: string to find ocurrrence
+* @needle: string ocurrence
+*
+* Return: a pointer to the beginning of the located substring or NULL if the
+* if the substring is not found.
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j = 0, x, count = 0;
+
+	for (x = 0; needle[x] != '\0'; x++)
+	{
+	}
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[j])
+	{
+		j++;
+		count++;
+	}
+	else if (j >= x)
+	{
+		break;
+	}
+	}
+
+	if(count == 0)
+		return '\0';
+
+	return needle;
+}
