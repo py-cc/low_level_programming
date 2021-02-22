58530914064f4b533ff7484af91b21ba2dca3c8d
py-cc <2227@holbertonschool.com>
2021-02-22 04:15:32 +0000

    add file to check

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,49 +1,26 @@
 #include "holberton.h"
 /**
 * print_number - prints an integer
-* @n: check number
 *
-* Return: Always 0 (Success)
+* @n: num to check
 */
 void print_number(int n)
 {
-unsigned int num;
-if (n > 0)
-{
-if (n > 100 && n < 1000)
-{
-_putchar(n / 100 + '0');
-num = n / 10;
-_putchar(num % 10 + '0');
-_putchar(n % 10 + '0');
-}
-else if (n > 1000)
-{
-_putchar(n / 1000 + '0');
-num = n / 100;
-_putchar(num % 10 + '0');
-num = n % 100;
-_putchar(num / 10 + '0');
-_putchar(num % 10 + '0');
-}
-else
-{
-_putchar(n / 10 + '0');
-num = n % 10;
-_putchar(num + '0');
-}
-}
-else if (n < 0)
-{
-n = -n;
-_putchar('-');
-num = n / 10;
-_putchar(num + '0');
-num = n % 10;
-_putchar(num + '0');
-}
-else if (n == 0)
-{
-_putchar(n + '0');
-}
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
+	if (num / 10)
+	{
+		print_number(num / 10);
+	}
+
+	_putchar(num % 10 + '0');
 }
