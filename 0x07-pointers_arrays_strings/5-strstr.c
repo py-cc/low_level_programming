8c4ba90eff74be753fd9d45e74ef96dd77fb0ea4
py-cc <2227@holbertonschool.com>
2021-02-23 00:40:23 +0000

    test

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,7 +11,7 @@ char *_strstr(char *haystack, char *needle)
 {
 	unsigned int i = 0, j = 0;
 
-	while (haystack[i])
+	while (haystack[i] != '\0')
 	{
 		while (needle[j] && (haystack[i] == needle[0]))
 		{
