394a3ce771c0b2d5acc16d91be739ff0fca61b6f
py-cc <2227@holbertonschool.com>
2021-02-24 23:33:06 +0000

    add file to check

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+* _print_rev_recursion - prints a string in reverse with recursion
+* @s: pointer to a string
+*
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+	}
+
+	if (*s != '\n')
+	{
+		s = s - 1;
+		_putchar(*s);
+	}
+	else
+	{
+		return;
+	}
+}
