d8063a34dd6f118a94675f8b79e51c7f93357526
py-cc <2227@holbertonschool.com>
2021-02-24 23:46:59 +0000

    add file to check

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,13 +11,13 @@ void _print_rev_recursion(char *s)
 		_print_rev_recursion(s + 1);
 	}
 
-	if (*s != '\n')
+	if (*s == '\0' || *s != '\n')
 	{
-		s = s - 1;
-		_putchar(*s);
+		_putchar(*s++);
 	}
 	else
 	{
+		_putchar('\n');
 		return;
 	}
 }
