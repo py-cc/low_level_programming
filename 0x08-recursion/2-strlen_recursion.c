ce3239b83a3a794d7bf25d540aedd628dcd5d1be
py-cc <2227@holbertonschool.com>
2021-02-25 00:50:18 +0000

    style betty

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 /**
-* _strlen_recursion - returns the length of a string
-*
+* _strlen_recursion - length of a string
 * @s: pointer to string
+* Return: length of a string
 */
 int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		return 1 + _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
 	return (0);
 }
