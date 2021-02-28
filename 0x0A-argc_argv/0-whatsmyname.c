d24ce528853b534dfa42b56e93d942562aeac7f9
py-cc <2227@holbertonschool.com>
2021-02-28 15:51:09 +0000

    add file to check

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+*  main - Entry point
+* @argc: size of argv
+* @argv: size of command arguments
+*
+* Return: 0 if success
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
