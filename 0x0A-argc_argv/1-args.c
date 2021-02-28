15236697bb33d994b59ff440ac962687cb134159
py-cc <2227@holbertonschool.com>
2021-02-28 16:00:48 +0000

    add file to check

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+* main - Entry point
+* @argc: size of argv
+* @argv: size of command arguments
+* Return: 0 Sucess
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	}
+
+	i = i - 1;
+
+	printf("%d\n", i);
+	return (0);
+}
