53b61a13bae95e591f2074c3d015153bce83702b
py-cc <2227@holbertonschool.com>
2021-03-02 23:51:40 +0000

    add file to check

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* str_concat - concatenates two strings
+* @s1: firts string
+* @s2: second string
+*
+* Return: pointer to a new memory space storing two strings
+* if it passed NULL return string empty or NULL in case malloc failed
+*/
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+	int i, j, k, y, n;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return ("");
+	}
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
+
+	for (k = 0; s2[k] != '\0'; k++)
+	{
+	}
+
+	n = k + i;
+
+	ptr = malloc(n*sizeof(char) + 1);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (j = 0; s1[j] != '\0'; j++)
+	{
+		ptr[j] = s1[j];
+	}
+
+	for (y = 0; s2[y] != '\0'; y++)
+	{
+		ptr[i + y] = s2[y];
+	}
+
+	ptr[i + k] = '\0';
+
+	return (ptr);
+}
