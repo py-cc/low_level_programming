abab41828de6c6db18dfcb4047fc2b1e4a19bc7e
py-cc <2227@holbertonschool.com>
2021-03-02 22:45:38 +0000

    function NULL

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,30 +14,27 @@ char *_strdup(char *str)
 	char *ptr;
 	unsigned int i, j;
 
-	if (str != NULL)
-	{
-		for (i = 0; str[i] != '\0'; i++)
-		{
-		}
-
-		ptr = malloc(i * sizeof(char) + 1);
-
-		for (j = 0; j <= i; j++)
-		{
-			ptr[j] = str[j];
-		}
-
-		if (ptr == NULL)
-		{
-			return (NULL);
-		}
-		else
-		{
-			return (ptr);
-		}
-	}
-	else
+	if (str == NULL)
 	{
 		return (NULL);
 	}
+
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+
+	ptr = malloc(i * sizeof(char) + 1);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (j = 0; j <= i; j++)
+	{
+		ptr[j] = str[j];
+	}
+
+	return (ptr);
 }
