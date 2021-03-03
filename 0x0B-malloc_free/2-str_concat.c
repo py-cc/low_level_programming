4b3d2e66782bf5c8557d2bcfc3d8b98a8657b121
py-cc <2227@holbertonschool.com>
2021-03-03 02:17:40 +0000

    add file to check

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,37 +16,38 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL || s2 == NULL)
 	{
-		return "";
+		return ("");
 	}
-
-	for (i = 0; s1[i] != '\0'; i++)
+	else
 	{
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+		}
+
+		for (k = 0; s2[k] != '\0'; k++)
+		{
+		}
+
+		n = k + i;
+
+		ptr = malloc((n * sizeof(char)) + 1);
+
+		if (ptr == NULL)
+		{
+			return (NULL);
+		}
+
+		for (j = 0; s1[j] != '\0'; j++)
+		{
+			ptr[j] = s1[j];
+		}
+
+		for (y = 0; s2[y] != '\0'; y++)
+		{
+			ptr[i + y] = s2[y];
+		}
+
+		ptr[i + k] = '\0';
 	}
-
-	for (k = 0; s2[k] != '\0'; k++)
-	{
-	}
-
-	n = k + i;
-
-	ptr = malloc((n * sizeof(char)) + 1);
-
-	if (ptr == NULL)
-	{
-		return (NULL);
-	}
-
-	for (j = 0; s1[j] != '\0'; j++)
-	{
-		ptr[j] = s1[j];
-	}
-
-	for (y = 0; s2[y] != '\0'; y++)
-	{
-		ptr[i + y] = s2[y];
-	}
-
-	ptr[i + k] = '\0';
-
 	return (ptr);
 }
