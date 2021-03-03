5d479e1063d9160aacbb5ebea78adf94decfbe4a
py-cc <2227@holbertonschool.com>
2021-03-03 02:06:23 +0000

    add file to check

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,3 +1,8 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+
+#endif /* _HOLBERTON_H_ */
