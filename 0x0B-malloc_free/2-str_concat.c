6530d3a1942df46d011de74c9887111fb81aeb59
py-cc <2227@holbertonschool.com>
2021-03-03 02:14:39 +0000

    add file to check

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,24 +14,17 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 	unsigned int i, j, k, y, n;
 
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
-	if (s1 != NULL)
+	if (s1 == NULL || s2 == NULL)
 	{
-		for (i = 0; s1[i] != '\0'; i++)
-		{
-		}
+		return "";
 	}
 
-	if (s2 != NULL)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
-		for (k = 0; s2[k] != '\0'; k++)
-		{
-		}
+	}
+
+	for (k = 0; s2[k] != '\0'; k++)
+	{
 	}
 
 	n = k + i;
