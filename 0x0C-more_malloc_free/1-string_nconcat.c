6783c61c1518d7ef3edce21dc1e99f838933ce44
py-cc <2227@holbertonschool.com>
2021-03-04 19:59:24 +0000

    fix style betty

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -37,7 +37,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			buffer[str] = s1[str];
 		for (str2 = 0; str2 <= j; str2++)
 			buffer[str + str2] = s2[str2];
-		return(buffer);
+		return (buffer);
 	}
 
 	buffer = malloc(n + i + 1 * sizeof(char));
