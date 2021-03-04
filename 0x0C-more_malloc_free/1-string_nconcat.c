c5dceef3502acb466bddde22ff0747c383d9cb07
py-cc <2227@holbertonschool.com>
2021-03-04 19:47:41 +0000

    add file to check

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,55 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* string_nconcat - concatenates two string with memory dinamic
+* @s1: string base
+* @s2: string to concatenates
+* @n: number of bytes
+*
+* Return: NULL if buffer failure, or  pointer shall point to a newly
+* allocated space in memory with succes
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i = 0, j = 0, str, str2 = 0;
+	char *buffer;
+
+	if (s1 != NULL)
+	{
+		while (s1[i])
+			i++;
+	}
+	if (s2 != NULL)
+	{
+		while (s2[j])
+			j++;
+	}
+	else
+		s2 = "";
+
+	if (n < j)
+	{
+		buffer = malloc(n + i + 1 * sizeof(char));
+		if (buffer == NULL)
+			return (NULL);
+	}
+	else if (n >= j)
+	{
+		buffer = malloc(i + j + 1 * sizeof(char));
+		if (buffer == NULL)
+			return (NULL);
+	}
+
+	for (str = 0; str < i; str++)
+	{
+		buffer[str] = s1[str];
+	}
+	for (str2 = 0; str2 < n; str2++)
+	{
+		buffer[str + str2] = s2[str2];
+	}
+		buffer[str2 + str] = '\0';
+
+	return (buffer);
+}
