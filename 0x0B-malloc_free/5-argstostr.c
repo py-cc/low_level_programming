b555d3e2b6791eaa8f1fb3c0d4ce3e4098e56c89
py-cc <2227@holbertonschool.com>
2021-03-04 01:32:39 +0000

    add file to check

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* argstostr -  concatenates all the arguments of your program
+* @ac: size, totally number of arguments
+* @av: content of arguments
+*
+*/
+char *argstostr(int ac, char **av)
+{
+	char *arr;
+	int i = 0, j, l = 0, p = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	while (av[i])
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			l++;
+			j++;
+		}
+		i++;
+	}
+	arr = malloc(l + ac + 1);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			arr[p] = av[i][j];
+			p++;
+		}
+		arr[p] = '\n';
+		p++;
+	}
+	arr[p] = '\0';
+	return (arr);
+}
