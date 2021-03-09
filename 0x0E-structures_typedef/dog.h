336be2ec06cfae7cd0ec4fc79555f7af4cd18656
py-cc <2227@holbertonschool.com>
2021-03-09 00:53:47 +0000

    add file to check

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+* struct dog - define struct
+* @name: pointer type char
+* @age: number type float
+* @owner: pointer type char
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */
