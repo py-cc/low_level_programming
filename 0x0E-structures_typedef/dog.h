6805aa85503d6fd9952d441e26f2beefe2d0a805
py-cc <2227@holbertonschool.com>
2021-03-09 03:12:16 +0000

    add file to check

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,12 +6,12 @@
 * @age: number type float
 * @owner: pointer type char
 */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
