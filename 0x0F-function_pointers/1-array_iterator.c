18df9c2f1f9fc40dc846fe2c067d8e2f0d76d4ad
py-cc <2227@holbertonschool.com>
2021-03-10 22:49:43 +0000

    fix condition NULL

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 /**
-* array_iterador - executes array with pointer to function with print
+* array_iterator - executes array with pointer to function with print
 * @array: pointer to array
 * @size: size to array
 * @action: pointer to function with print
@@ -9,10 +9,14 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
 
-	for (i = 0; i < size; i++)
+	if (action != NULL)
 	{
-		action(array[i]);
+		unsigned int i;
+
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 	}
 }
