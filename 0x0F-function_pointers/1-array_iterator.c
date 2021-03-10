e514b8a4dc30dc1dc9fa6e56fc9a4a21f7ee47dc
py-cc <2227@holbertonschool.com>
2021-03-10 22:57:45 +0000

    fix comparison between pointer and integer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,7 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 
-	if (action != NULL && size != NULL && array != NULL)
+	if (array != NULL && size != 0 && action != NULL)
 	{
 		unsigned int i;
 
