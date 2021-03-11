368334587aa170d7c48918459b67627c892b9962
py-cc <2227@holbertonschool.com>
2021-03-11 04:17:55 +0000

    fix to condition

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,20 +18,13 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	if (!get_op_func(argv[2]) || argv[2][1] != '\0')
+	if ((argv[2][0] != '+' && argv[2][0] != '-' &&
+		argv[2][0] != '*' && argv[2][0] != '/'
+		&& argv[2][0] != '%') || (argv[2][1] != '\0'))
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	/**
-	*if ((argv[2][0] != '+' && argv[2][0] != '-'
-	*&& argv[2][0] != '*' && argv[2][0]
-	*!= '/' && argv[2][0] != '%') || (argv[2][1] != '\0'))
-	*{
-	*printf("Error\n");
-	*exit(99);
-	*}
-	*/
 	if ((argv[2][0] == '%' || argv[2][0] == '/') && (atoi(argv[3]) == 0))
 	{
 		printf("Error\n");
