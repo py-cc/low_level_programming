5dc6529628fb90cf8d9f79ccab1cf5199b4889dc
py-cc <2227@holbertonschool.com>
2021-03-11 04:02:20 +0000

    add file to check

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - program for calculating simple operations
+* @argv: number arguments
+* @argc: string commands lines
+*
+* Return: 0 Success
+*/
+int main(int argc, char *argv[])
+{
+	int num1, num2, calc;
+	char *sign;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*' &&
+		argv[2][0] != '/' && argv[2][0] != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2][0] == '%' || argv[2][0] == '/') && (atoi(argv[3]) == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+		num1 = atoi(argv[1]);
+		sign = argv[2];
+		num2 = atoi(argv[3]);
+
+		calc = get_op_func(sign)(num1, num2);
+		printf("%d\n", calc);
+		return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+/**
+* op_add - operation sum two integers
+* @a: first number
+* @b: second number
+*
+* Return: result of sum
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+* op_sub - operation difference two integers
+* @a: first number
+* @b: second number
+*
+* Return: result of difference
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+* op_mul - operation multiplication two integers
+* @a: first number
+* @b: second number
+*
+* Return: result of product
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+* op_div - operation division two integers
+* @a: first number
+* @b: second number
+*
+* Return: result of division
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+* op_mod - operation remainder of the division in two integers
+* @a: first number
+* @b: second number
+*
+* Return: result of remainder
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
