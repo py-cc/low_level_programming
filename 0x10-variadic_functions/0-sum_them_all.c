456924f71d6a24fc0afcdcbef842dcf590b91a9b
py-cc <2227@holbertonschool.com>
2021-03-11 17:36:20 +0000

    fix style betty

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,9 +11,10 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i, sum = 0;
 	va_list args;
-	va_start(args,n);
 
-	if(n == 0)
+	va_start(args, n);
+
+	if (n == 0)
 	return (0);
 
 	for (i = 0; i < n; i++)
