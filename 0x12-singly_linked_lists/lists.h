f5f60fa5ebfed7caef21c9e2dd11b05608a27664
py-cc <paolacarrero731@gmail.com>
2021-03-19 17:51:52 +0000

    fix style betty

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,5 @@
 #ifndef _LIST_H_
 #define _LIST_H_
-
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -14,13 +13,13 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
+
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
-
 #endif /* _LIST_H_ */
