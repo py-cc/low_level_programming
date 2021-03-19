a12a9ca970c575556deb7c3618a6df93183e5faf
py-cc <paolacarrero731@gmail.com>
2021-03-19 17:47:44 +0000

    fix style betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,11 +1,10 @@
 #include "lists.h"
 /**
-*
-*
-*
-*
-*
-*/
+ * print_list - prints all the elements of a list.
+ * @h: list to print.
+ *
+ * Return: the number of nodes.
+ */
 size_t print_list(const list_t *h)
 {
 	size_t i;
@@ -14,14 +13,14 @@ size_t print_list(const list_t *h)
 
 	if (h)
 	{
-		while(h != NULL)
+		while (h != NULL)
 		{
-			if(h->str == NULL)
+			if (h->str == NULL)
 				printf("[0] (nil)\n");
 			else
 				printf("[%u] %s\n", h->len, h->str);
 			h = h->next;
-			i++;
+		Ã	i++;
 		}
 	}
 
