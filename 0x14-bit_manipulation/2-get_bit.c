ed7bf6ac7dc58b23467819fbc7ea457275c7133a
py-cc <paolacarrero731@gmail.com>
2021-03-26 03:49:21 +0000

    add file to check

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index.
+ * @n: number to fin index in.
+ * @index: position to retrieve bit from n.
+ *
+ * Return: the value of the bit at index index or -1 if an error occured.
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+
+	if (index < sizeof(n) * 8)
+	{
+		for (i = 0; i < index; i++)
+			n = n >> 1;
+		if (n & 1)
+			return (1);
+		else
+			return (0);
+	}
+	return (-1);
+}
