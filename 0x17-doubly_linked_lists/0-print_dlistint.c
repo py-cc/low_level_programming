3ebf47102065b1ba3b352b22445b5e0128eccbfc
py-cc <paolacarrero731@gmail.com>
2021-05-02 18:19:23 -0500

    fix style betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "lists.h"
-
+/**
+ * print_dlistint - function print nodes
+ * @h: head to double linked list
+ *
+ * Return: the number of nodes.
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	dlistint_t const *current = h;
