582e1423be8f5a75e59962c8541ea2d30fb45a99
py-cc <paolacarrero731@gmail.com>
2021-05-02 18:40:36 -0500

    fix comment function

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "lists.h"
-
+/**
+ * dlistint_len - Returns number of elements in a double linked list.
+ * @h: Head of the double linked list.
+ *
+ *
+ * Return: the number of nodes.
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t num_nodes = 0;
