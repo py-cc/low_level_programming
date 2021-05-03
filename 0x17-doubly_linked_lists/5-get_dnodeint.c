d1f930b12f6fa8c6ca9a64b7c7325e5987d66e33
py-cc <paolacarrero731@gmail.com>
2021-05-02 20:32:11 -0500

    add file to check funtion get node

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * get_dnodeint_at_index - Function returns nth node of double linked list.
+ * @head: Head of the double linked list.
+ * @index: Index of the node, starting from 0.
+ *
+ * Return: address of nth node of double linked list. NULL if does not exist.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+
+	dlistint_t *tmp = head;
+	unsigned int i = 0;
+
+	dlistint_t *new_node = NULL;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+
+	while (i < index)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	new_node->n = tmp->n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+
+	return (new_node);
+}
