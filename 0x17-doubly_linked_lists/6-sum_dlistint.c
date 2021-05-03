1640e73d1218c12eb1d32207889bdacdcae0e678
py-cc <paolacarrero731@gmail.com>
2021-05-02 20:48:21 -0500

    fix description function

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "lists.h"
-
+/**
+ * sum_dlistint - Function returns sum of all the data of a double linked list.
+ * @head: Head of the double linked list.
+ *
+ * Return: The sum of all the data (n).
+ */
 int sum_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp = head;
